7fd4441dcd84a86f58d22d9f66dd28327145e1c8
mattgodmere <matthewgodmere@gmail.com>
2017-10-16 16:23:47 -0500

    Moving Image Test

--- test/testClient.cpp
+++ test/testClient.cpp
@@ -27,8 +27,8 @@ int main(int argc, char *argv[])
 
 	// create an image packet
 	cv::Mat iimage = cv::imread(image_path);
-	Coord center(0,1);
-	float zoom = 0;
+	Coord center(0,0);
+	float zoom = 2;
 
 	// find the type of image
 	int type = CV_8UC3;
@@ -38,12 +38,16 @@ int main(int argc, char *argv[])
 		type = ImagePacket::ImageType::BGR24;
 	else if(iimage.type() == CV_8UC4)
 		type = ImagePacket::ImageType::BGRA32;
-	ImagePacket ipacket(center, zoom, iimage.cols, iimage.rows, iimage.elemSize() * 8, type, iimage.data);
-	Message wmsg(MessageType::IMAGE, ipacket.GetPacket(), PROTO_COMPRESSION);
 
 	// send every 1 second
 	while(true)
 	{
+		center.lat += 10;
+		center.lat = fmodf(center.lat, 60);
+		center.lon += 10;
+		center.lon = fmodf(center.lon, 180);
+		ImagePacket ipacket(center, zoom, iimage.cols, iimage.rows, iimage.elemSize() * 8, type, iimage.data);
+		Message wmsg(MessageType::IMAGE, ipacket.GetPacket(), PROTO_COMPRESSION);
 		std::cout << "Sent " << wmsg.Get().size() << " bytes..." << std::endl;
 		client.Send(wmsg.Get());
 		s_sleep(1000);
